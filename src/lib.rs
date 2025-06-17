d49a61d533de3b1dfd1b4ea634f8b0bfcc7adedb
stewthestew <stew156@proton.me>
2025-06-17 05:06:54 +0000

    Added Clap

--- src/lib.rs
+++ src/lib.rs
@@ -2,21 +2,24 @@
 //!
 //! Namespaced (recommended):
 //! ```rust
-//! use console_stuff::{console, dialoguer, indicatif};
+//! use console_stuff::{console, dialoguer, indicatif, clap};
 //! console::style("hello").red();
 //! ```
 //!
 //! Or import everything:
 //! ```rust  
-//! use console_stuff::prelude::*;
+//! use console_stuff::prelude::*; // Bad idea, really bad idea, just use the namespaced way.
 //! style("hello").red();
 //! ```
 
+pub use clap;
 pub use console;
 pub use dialoguer;
 pub use indicatif;
 
+#[allow(ambiguous_glob_reexports)]
 pub mod prelude {
+    pub use clap::*;
     pub use console::*;
     pub use dialoguer::*;
     pub use indicatif::*;
