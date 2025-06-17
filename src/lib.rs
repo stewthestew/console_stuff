cefd64fb55f58e9230cd697676a0a88ced08bb37
stewthestew <stew156@proton.me>
2025-06-17 03:30:05 +0000

    init

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,23 @@
+//! # Usage
+//!
+//! Namespaced (recommended):
+//! ```rust
+//! use console_stuff::{console, dialoguer, indicatif};
+//! console::style("hello").red();
+//! ```
+//!
+//! Or import everything:
+//! ```rust  
+//! use console_stuff::prelude::*;
+//! style("hello").red();
+//! ```
+
 pub use console;
 pub use dialoguer;
 pub use indicatif;
+
+pub mod prelude {
+    pub use console::*;
+    pub use dialoguer::*;
+    pub use indicatif::*;
+}
